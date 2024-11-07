b504ce5b443a537e9dc59325232665d264f19b2c
Anastasiia Sevastianova <sevasyanova.anastasia1@gmail.com>
2024-11-07 14:07:39 +0100

    feat: implement  POST request(/decrypt) to API

--- libs/app.js
+++ libs/app.js
@@ -16,4 +16,16 @@ app.post("/encrypt", (req, res) => {
     res.json({ encryptedMessage });
 })
 
+app.post("/decrypt", (req, res) => {
+    // Take in message from request
+    const { message } = req.body;
+    // Respond with decrypted message
+    if (!message) {
+        return res.status(400).json({ error: 'Message required' });
+    }
+
+    const decryptedMessage = decrypt(message);
+    res.json({ decryptedMessage });
+})
+
 export default app;
